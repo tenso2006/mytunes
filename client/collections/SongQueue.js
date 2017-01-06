4394fe58c8e698673950be91d8d1bda87e2b9cd8
tenso2006 <tenso2006@gmail.com>
2017-01-05 18:15:36 -0800

    done: song model

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', this.playFirst, this);
 
+    this.on('ended', this.playNext, this);
+
+    this.on('dequeue', this.dequeue, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  playNext: function () {
+    this.shift();
+    if (this.length > 0) {
+      this.playFirst();
+    } else {
+      this.trigger('stop');
+    }
+  },
+
+  dequeue: function (song) {
+    this.remove(song);
+  }
 });
